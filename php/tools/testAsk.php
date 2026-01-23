bb46106dcc665362c4e48fc0310e9466baa0bc9a
Brian Tawney <btawney@manjurist.net>
2026-01-23 13:40:11 -0800

    Updates

---
+++ php/tools/testAsk.php
@@ -0,0 +1,24 @@
+<?php // testAsk.php
+
+require_once('../deepseek.inc.php');
+
+$question = '';
+
+foreach ($argv as $arg) {
+	$question .= $arg . ' ';
+}
+
+$session = new \Deepseek\Session()->echo()
+  ->onError(function($e) {
+  	print "ERROR: $e\n";
+  })
+  ->onLimitExceeded(function ($limit) {
+  	print "LIMIT EXCEEDED: $limit\n";
+  });
+
+$startTime = time();
+
+$session->ask($question);
+
+print "Elapsed Seconds: " . (time() - $startTime) . "\n";
+print "Usage:           " . $session->usage() . "\n";
