4a85d7a1bfbc46fac4400e22fc66ff92b4f396d2
Brian Tawney <btawney@manjurist.net>
2026-02-08 15:52:43 -0800

    Added text structures to translation log

--- php/translationLog.inc.php
+++ php/translationLog.inc.php
@@ -1,5 +1,7 @@
 <?php // translationLog.inc.php
 
+require_once('parseAnnotations.inc.php');
+
 function appendLog($state, $outputFile) {
     // Write to output file 
     $data = array(
@@ -97,4 +99,170 @@ class LogEntry {
 	var $translation;
 	var $properNouns;
 	var $consecutiveErrorCount;
+}
+
+class TranslatedText {
+    var $summary;
+    var $fascicles;
+    var $index;
+
+    function __construct() {
+        $this->summary = false;
+        $this->fascicles = array();
+        $this->index = array();
+    }
+
+    static function fromLog($path) {
+	    $entries = readLog($path);
+	    $text = new TranslatedText();
+
+	    $currentFascicle = false;
+
+	    foreach ($entries as $entry) {
+  	        $text->summary = $entry->textSummary;
+
+	        if ($currentFascicle == false || $currentFascicle->name != $entry->fascicleName) {
+	            $currentFascicle = new TranslatedFascicle($entry->fascicleName);
+	            $text->fascicles[] = $currentFascicle;
+	        }
+
+	        $currentFascicle->summary = $entry->fascicleSummary;
+	        $currentParagraph = new TranslatedParagraph($entry->paragraphNumber);
+	        $currentFascicle->paragraphs[] = $currentParagraph;
+
+	        $tLines = explode("\n", $entry->translation);
+	        $inNotes = false;
+	        foreach ($tLines as $tLine) {
+		        $tLine = trim($tLine);
+
+		        if ($tLine == 'END_OF_TRANSLATION') {
+		            $inNotes = true;
+		            continue;
+		        } elseif (strlen($tLine) == 0) {
+		            continue;
+		        }
+
+		        if ($inNotes) {
+		            $currentParagraph->notes[] = $tLine;
+		        } else {
+		            $currentParagraph->translation[] = $tLine;
+		        }
+	        }
+
+	        $parsed = parseAnnotation($entry->properNouns);
+
+	        foreach ($result as $item) {
+	        	$text->appendIndexEntry($item->source, $item->type, $item->target, $currentParagraph, $item->notes);
+	        }
+	    }
+
+	    return $text;
+    }
+
+    function appendIndexEntry($name, $type, $translation, $paragraph, $notes) {
+    	if (empty($this->index[$name])) {
+    		$entry = new IndexEntry($name);
+    		$this->index[$name] = $entry;
+    	} else {
+    		$entry = $this->index[$name];
+    	}
+
+    	$entry->append($type, $translation, $paragraph, $notes);
+    }
+}
+
+class TranslatedFascicle {
+    var $name;
+    var $summary;
+    var $paragraphs;
+
+    function __construct($name) {
+        $this->name = $name;
+        $this->summary = false;
+        $this->paragraphs = array();
+    }
+}
+
+class TranslatedParagraph {
+    var $number;
+	var $translation;
+	var $notes;
+
+	function __construct($number) {
+	    $this->number = $number;
+	    $this->translation = array();
+	    $this->notes = array();
+	}
+}
+
+class IndexEntry {
+	var $name;
+	var $types;
+
+	function __construct($name) {
+		$this->name = $name;
+		$this->types = array();
+	}
+
+	function append($type, $translation, $paragraph, $notes) {
+		if (empty($this->types[$type])) {
+			$entry = new IndexEntryType($type);
+			$this->types[$type] = $entry;
+		} else {
+			$entry = $this->types[$type];
+		}
+
+		$entry->append($translation, $paragraph, $notes);
+	}
+}
+
+class IndexEntryType {
+	var $type;
+	var $translations;
+
+	function __construct($type) {
+		$this->type = $type;
+		$this->translations = array();
+	}
+
+	function append($translation, $paragraph, $notes) {
+		if (empty($this->translations[$translation])) {
+			$entry = new IndexEntryTranslation($translation);
+			$this->translations[$translation] = $entry;
+		} else {
+			$entry = $this->translations[$translation];
+		}
+
+		$entry->append($paragraph, $notes);
+	}
+}
+
+class IndexEntryTranslation {
+	var $translation;
+	var $notes;
+	var $paragraphs;
+
+	function __construct($translation) {
+		$this->translation = $translation;
+		$this->notes = array();
+		$this->paragraphs = array();
+	}
+
+	function append($paragraph, $notes) {
+		$this->paragraphs[] = $paragraph;
+
+		if (strlen($notes) > 0) {
+			$exists = false;
+			foreach ($this->notes as $note) {
+				if ($note == $notes) {
+					$exists = true;
+					break;
+				}
+			}
+		}
+
+		if (!$exists) {
+			$this->notes[] = $notes;
+		}
+	}
 }
